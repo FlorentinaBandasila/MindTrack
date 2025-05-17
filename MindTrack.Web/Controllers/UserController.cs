359a4dcace56d21f3827bff5b288e1c1e7a39806
FlorentinaBandasila <florentina.bandasila@student.upt.ro>
2025-05-17 03:49:44 +0300

    add authorize

--- MindTrack.Web/Controllers/UserController.cs
+++ MindTrack.Web/Controllers/UserController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MindTrack.Web.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
